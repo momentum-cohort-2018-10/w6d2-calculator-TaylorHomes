767a0a1d109a977c60f77dc1b8e86b91b288e552
TaylorHomes <taylor.homes01@gmail.com>
2018-12-05 16:33:35 -0500

    Calculator works, very ugly.

--- main.js
+++ main.js
@@ -1,65 +1,73 @@
-function click1(event) {
-    let thing = document.getElementsByClass("calculator_keys")
-    let display = document.getElementsByClass("calculator_display")
-    let newDisplay = document.createElement('keys')
-    thing.onclick = function () {
-        newDisplay.innerText = "1"
-        display.appendChild(newDisplay)
-    }
+document.getElementById('seven').addEventListener('click', function () {
+    document.getElementById('display').innerText += '7'
+})
+
+document.getElementById('eight').addEventListener('click', function () {
+    document.getElementById('display').innerText += '8'
+})
+
+document.getElementById('nine').addEventListener('click', function () {
+    document.getElementById('display').innerText += '9'
+})
+
+document.getElementById('four').addEventListener('click', function () {
+    document.getElementById('display').innerText += '4'
+})
+
+document.getElementById('five').addEventListener('click', function () {
+    document.getElementById('display').innerText += '5'
+})
+
+document.getElementById('six').addEventListener('click', function () {
+    document.getElementById('display').innerText += '6'
+})
+
+document.getElementById('one').addEventListener('click', function () {
+    document.getElementById('display').innerText += '1'
+})
+
+document.getElementById('two').addEventListener('click', function () {
+    document.getElementById('display').innerText += '2'
+})
+
+document.getElementById('three').addEventListener('click', function () {
+    document.getElementById('display').innerText += '3'
+})
+
+document.getElementById('zero').addEventListener('click', function () {
+    document.getElementById('display').innerText += '0'
+})
+
+document.getElementById('decimal').addEventListener('click', function () {
+    document.getElementById('display').innerText += '.'
+})
+
+document.getElementById('plus').addEventListener('click', function () {
+    document.getElementById('display').innerText += '+'
+})
+
+document.getElementById('minus').addEventListener('click', function () {
+    document.getElementById('display').innerText += '-'
+})
+
+document.getElementById('multiply').addEventListener('click', function () {
+    document.getElementById('display').innerText += '*'
+})
+
+document.getElementById('divide').addEventListener('click', function () {
+    document.getElementById('display').innerText += '%'
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+    display = "";
+    document.getElementById('display').innerText = display
+})
+
+
+function equal() {
+    document.getElementById('display').innerText = eval(document.getElementById('display').innerText)
 }
-click1()
 
 
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// let buttons = document.getElementsByClassName('num');
-// for (let button of buttons) {
-//     button.addEventListener("click", clickHandler);
-
-// document.getElementById("seven").addEventListener("click", myFunction);
-
-// var seven = document.getElementsByClassName("num");
-// seven.addEventListener("click", myFunction);
-
-// function myFunction() {
-//     console.log("hello world");
-// }
-
-
-// var element = document.getElementById(id);
-
-// function printSomething(number) {
-//     var elem = document.getElementById('seven');
-//     print = var elem
-
-// }
-
-
-// var buttons = document.getElementsByClassName("num");
-// for (let button in buttons) (
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("num").addEventListender("click", function () {
-//     document.getElementById("seven").innerHTML = "7";
-
-// });
-
-
-
-// Var calculator = document.getElementById(“display”);
-// function clickCalcButton(el) {
-//     calculator.innerHTML += el.innerHTML;
-// }
-// function clickEqualsButton() {
-//     calculator.innerHTML += eval(calculator.innerHTML);
-// }
 
